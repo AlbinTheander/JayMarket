464d4264c7b1c6b7f9046f5ea7c5eabf30738bdd
Karin Hofbauer <karin.hofbauer@jayway.com>
2010-09-05 19:21:22 +0200

    file upload via rest

--- rest-api/src/main/java/com/jayway/jaymarket/ApplicationService.java
+++ rest-api/src/main/java/com/jayway/jaymarket/ApplicationService.java
@@ -3,22 +3,22 @@ package com.jayway.jaymarket;
 
 import com.jayway.jaymarket.dto.Application;
 import com.jayway.jaymarket.dto.Applications;
+import com.sun.jersey.core.header.ContentDisposition;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataMultiPart;
+import org.grlea.log.SimpleLogger;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-/** Example resource class hosted at the URI path "/myresource"
+/**
  */
 @Path("/")
 public class ApplicationService {
-    
-    /** Method processing HTTP GET requests, producing "text/plain" MIME media
-     * type.
-     * @return String that will be send back as a response of type "text/plain".
-     */
+    public static SimpleLogger log = new SimpleLogger(ApplicationService.class);
+
     @GET
     @Path("/applications")
     @Produces("text/xml")
@@ -30,5 +30,57 @@ public class ApplicationService {
         applications.add(new Application("ByeBye  App"));
 
         return new Applications(applications);
+    }
+
+    //TODO: check that file is an apk file
+    //TODO: save filename, url and description in database
+    @POST
+    @Path("/upload")
+    @Consumes("multipart/form-data")
+    public String upload(FormDataMultiPart formData) {
+        FormDataBodyPart namePart = formData.getField("name");
+        FormDataBodyPart descriptionPart = formData.getField("description");
+        FormDataBodyPart file = formData.getField("file");
+
+        ContentDisposition disposition = file.getContentDisposition();
+        String fileName = disposition.getFileName();
+
+
+        String title = namePart.getValueAs(String.class);
+        String description = descriptionPart.getValueAs(String.class);
+        
+        BufferedInputStream bufferedInput = new BufferedInputStream(file.getValueAs(InputStream.class));
+        BufferedOutputStream bufferedOut = null;
+
+        try {
+            bufferedOut = new BufferedOutputStream(new FileOutputStream(fileName),1024);
+            byte[] data = new byte[1024];
+            int counter;
+
+            while ((counter = bufferedInput.read(data)) != -1) {
+                bufferedOut.write(data,0,counter);
+            }
+            log.debug("Successfully written application "+fileName);
+
+        } catch (FileNotFoundException e) {
+            log.error("Could not write to file "+fileName);
+            log.errorException(e);
+        } catch (IOException e) {
+            log.error("Exception when writing to file "+fileName);
+            log.errorException(e);
+        } finally {
+            close(bufferedOut);
+        }
+
+
+        return "super";
+    }
+
+    private void close(Closeable closeable) {
+        if(closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {/* What would you do here? */}
+        }
     }
 }
