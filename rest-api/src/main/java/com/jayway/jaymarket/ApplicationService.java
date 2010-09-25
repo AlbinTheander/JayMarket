3fb819dd87329754a29b9fdd34c81035a5986e67
Albin Theander <Albin.Theander@jayway.com>
2010-09-25 10:10:36 +0200

    Put back Produces annotation.

--- rest-api/src/main/java/com/jayway/jaymarket/ApplicationService.java
+++ rest-api/src/main/java/com/jayway/jaymarket/ApplicationService.java
@@ -13,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.io.IOUtil;
@@ -37,6 +38,7 @@ public class ApplicationService {
 
 	@GET
 	@Path("/applications")
+	@Produces({ MediaType.TEXT_XML, MediaType.APPLICATION_JSON })
 	public Applications fetchThemAll() {
 		return repo.getApplications();
 	}
