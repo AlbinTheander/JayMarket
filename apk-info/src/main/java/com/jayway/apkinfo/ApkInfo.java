16e7a4c95e8f7a08d542fc053e9e939a3ec6cb1d
Albin Theander <Albin.Theander@jayway.com>
2010-09-26 14:00:16 +0200

    Made some progress on the ApkInfo utility. Still not working.

--- apk-info/src/main/java/com/jayway/apkinfo/ApkInfo.java
+++ apk-info/src/main/java/com/jayway/apkinfo/ApkInfo.java
@@ -1,10 +1,41 @@
 package com.jayway.apkinfo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ApkInfo {
 
-	public static ApkInfo getInfo(String apkPath) {
+	private static String aaptPath;
 
-		return new ApkInfo();
+	public static void setAaptPath(String aaptPath) {
+		ApkInfo.aaptPath = aaptPath;
 	}
 
+	public static String getAaptPath() {
+		// TODO: Should make some guessing the location of aapth if the path is
+		// null
+		return aaptPath;
+	}
+
+	public static ApkInfo getInfo(String apkPath) {
+		String aapt = ApkInfo.getAaptPath();
+		try {
+			Process p = Runtime.getRuntime().exec(
+					new String[] { aapt, "dump", "badging", apkPath });
+			BufferedReader aaptOutput = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+			p.waitFor();
+			String line;
+			while ((line = aaptOutput.readLine()) != null) {
+				System.out.println(line);
+			}
+			aaptOutput.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return new ApkInfo();
+	}
 }
--- apk-info/src/test/java/com/jayway/apkinfo/ApkInfoTest.java
+++ apk-info/src/test/java/com/jayway/apkinfo/ApkInfoTest.java
@@ -2,13 +2,44 @@ package com.jayway.apkinfo;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ApkInfoTest {
 
+	private static File apkFile;
+
+	@BeforeClass
+	public static void copyApkTestFileToTmp() throws IOException {
+		InputStream in = ApkInfoTest.class.getResourceAsStream("/Hello1.apk");
+		apkFile = File.createTempFile("Hello1", "apk");
+		FileOutputStream out = new FileOutputStream(apkFile);
+		byte[] buffer = new byte[14000];
+		int count = 0;
+		count = in.read(buffer);
+		while (count >= 0) {
+			out.write(buffer, 0, count);
+			count = in.read(buffer);
+		}
+		in.close();
+		out.close();
+		apkFile.deleteOnExit();
+		ApkInfo.setAaptPath("/Volumes/Shared/android/android-sdk-mac_86/platforms/android-4/tools/aapt");
+	}
+
+	@Test
+	public void apkFileExists() {
+		assertTrue(apkFile.exists());
+	}
+
 	@Test
 	public void canCreateApkInfo() {
-		assertNotNull(ApkInfo.getInfo(""));
+		assertNotNull(ApkInfo.getInfo(apkFile.getAbsolutePath()));
 	}
 
 }
